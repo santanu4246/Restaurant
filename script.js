d0a522f6383f1a390fa8401e6bce0c4c739caf07
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2024-05-08 18:12:19 +0530

    hero section

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuBtn = document.querySelector("#menu_btn");
-    const navLinks = document.querySelector("#nav-links");
-    const menuBtnIcon = menuBtn.querySelector("i");
+const menuBtn = document.querySelector("#menu_btn");
+const navLinks = document.querySelector("#nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-    menuBtn.addEventListener("click", function() {
-        navLinks.classList.toggle("open");
-        const isOpen = navLinks.classList.contains("open");
-        menuBtnIcon.className = isOpen ? "ri-close-line" : "ri-menu-line";
-    });
+//menubutton toggle
+menuBtn.addEventListener("click", function () {
+    navLinks.classList.toggle("open");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.className = isOpen ? "ri-close-line" : "ri-menu-line";
 });
+
+//click on navbar options
+navLinks.addEventListener("click",()=>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.className = "ri-menu-line";
+})
+
+const scrollRevealOption = {
+    distance:"50px",
+    origin:"bottom",
+    duration:1000
+};
+
+ScrollReveal().reveal(".header_image img",{
+    ...scrollRevealOption,
+    origin:"right"
+});
+ScrollReveal().reveal(".header_content h2",{
+    ...scrollRevealOption,
+    delay:500
+});
+ScrollReveal().reveal(".header_content h1",{
+    ...scrollRevealOption,
+    delay:1000
+});
+
+
